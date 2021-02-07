c15323b3d839f13a525265955d4ab52a5e26371e
nocarryr <matt@nomadic-recording.com>
2021-02-07 12:13:43 -0600

    Make the bind address configurable

--- src/jvconnected/interfaces/tslumd/umd_io.py
+++ src/jvconnected/interfaces/tslumd/umd_io.py
@@ -134,8 +134,13 @@ class UmdIo(Interface):
     """Main UMD interface
 
     Properties:
+        hostaddr (str): The local host address to bind the server to. Defaults
+            to ``0.0.0.0``
+        hostport (int): The port to listen on. Defaults to ``60000``
         tallies (Dict[int, Tally]): Mapping of :class:`Tally` objects using the
             :attr:`~Tally.index` as keys
+        config: Instance of :class:`jvconnected.config.Config`. This is gathered
+            from the :attr:`engine` after :meth:`set_engine` has been called.
 
     :Events:
         .. on_tally_added(tally: Tally)
@@ -146,28 +151,73 @@ class UmdIo(Interface):
 
             Fired when any :class:`Tally` instance has been updated
     """
+    hostaddr = Property('0.0.0.0')
+    hostport = Property(60000)
+    config = Property()
     tallies = DictProperty()
     _events_ = ['on_tally_added', 'on_tally_updated']
 
-    async def open(self):
-        if self.running:
+    def __init__(self):
+        self._reading_config = False
+        self._config_read = asyncio.Event()
+        self._connect_lock = asyncio.Lock()
+        super().__init__()
+        self.bind_async(self.loop, config=self.read_config)
+        self.bind(**{prop:self.update_config for prop in ['hostaddr', 'hostport']})
+
+    async def set_engine(self, engine: 'jvconnected.engine.Engine'):
+        if engine is self.engine:
             return
-        logger.debug('UmdIo.open()')
-        self.running = True
-        self.transport, self.protocol = await self.loop.create_datagram_endpoint(
-            lambda: UmdProtocol(self),
-            local_addr=('127.0.0.1', 60000),
-            reuse_port=True,
-        )
-        logger.success('UmdIo running')
+        if engine.config is not self.config:
+            self._config_read.clear()
+        self.config = engine.config
+        await self._config_read.wait()
+        await super().set_engine(engine)
+
+    async def open(self):
+        async with self._connect_lock:
+            if self.running:
+                return
+            logger.debug('UmdIo.open()')
+            self.running = True
+            self.transport, self.protocol = await self.loop.create_datagram_endpoint(
+                lambda: UmdProtocol(self),
+                local_addr=(self.hostaddr, self.hostport),
+                reuse_port=True,
+            )
+            logger.success('UmdIo running')
 
     async def close(self):
-        if not self.running:
+        async with self._connect_lock:
+            if not self.running:
+                return
+            logger.debug('UmdIo.close()')
+            self.running = False
+            self.transport.close()
+            logger.success('UmdIo closed')
+
+    async def set_bind_address(self, hostaddr: str, hostport: int):
+        """Set the :attr:`hostaddr` and :attr:`hostport` and restart the server
+        """
+        if hostaddr == self.hostaddr and hostport == self.hostport:
             return
-        logger.debug('UmdIo.close()')
-        self.running = False
-        self.transport.close()
-        logger.success('UmdIo closed')
+        running = self.running
+        if running:
+            await self.close()
+        self.hostaddr = hostaddr
+        self.hostport = hostport
+        if running:
+            await self.open()
+
+    async def set_hostaddr(self, hostaddr: str):
+        """Set the :attr:`hostaddr` and restart the server
+        """
+        await self.set_bind_address(hostaddr, self.hostport)
+
+    async def set_hostport(self, hostport: int):
+        """Set the :attr:`hostport` and restart the server
+        """
+        await self.set_bind_address(self.hostaddr, hostport)
 
     def parse_incoming(self, data: bytes, addr: Tuple[str, int]):
         """Parse data received by the server
@@ -192,6 +242,34 @@ class UmdIo(Interface):
         changed = tally.update_from_display(rx_display)
         if changed:
             self.emit('on_tally_updated', tally)
+
+    def update_config(self, *args, **kwargs):
+        """Update the :attr:`config` with current state
+        """
+        if self._reading_config:
+            return
+        config = self.config
+        if config is None:
+            return
+        if 'interfaces' not in config:
+            config['interfaces'] = {}
+        if 'tslumd' not in config['interfaces']:
+            config['interfaces']['tslumd'] = {}
+        d = config['interfaces']['tslumd']
+        d['hostaddr'] = self.hostaddr
+        d['hostport'] = self.hostport
+
+    async def read_config(self, *args, **kwargs):
+        config = self.config
+        if config is None:
+            return
+        d = config.get('interfaces', {}).get('tslumd', {})
+        self._reading_config = True
+        hostaddr = d.get('hostaddr', self.hostaddr)
+        hostport = d.get('hostport', self.hostport)
+        await self.set_bind_address(hostaddr, hostport)
+        self._reading_config = False
+        self._config_read.set()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
